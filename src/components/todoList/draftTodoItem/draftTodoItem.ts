08be894326f0ef9e712e2927bfd3fd5ac5d445ef
Natasha <75773163+natty-light@users.noreply.github.com>
2025-09-09 07:01:09 -0400

    draft to do item

--- src/components/todoList/draftTodoItem/draftTodoItem.ts
+++ src/components/todoList/draftTodoItem/draftTodoItem.ts
@@ -1,10 +1,19 @@
-import {Component, input} from '@angular/core';
+import {Component, input, signal, WritableSignal} from '@angular/core';
 import {ToDoItem} from '../../../types';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'draft-to-do-item',
+  imports: [
+    FormsModule
+  ],
   templateUrl: 'draftTodoItem.html'
 })
 export class DraftToDoItem {
-  item = input<ToDoItem>()
+  localItem = input.required<WritableSignal<Partial<Omit<ToDoItem, 'id'>>>>()
+  saveItem = input.required<() => void>()
+
+  onTextChange(event: Event) {
+    this.localItem().update((item) => ({...item, text: (event.target as HTMLInputElement).value}))
+  }
 }
