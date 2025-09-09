86bdd46066aecb2467558fc322ebf62c939de143
Natasha <75773163+natty-light@users.noreply.github.com>
2025-09-09 08:12:15 -0400

    editing items

--- src/components/todoList/todoItem/todoItem.ts
+++ src/components/todoList/todoItem/todoItem.ts
@@ -1,16 +1,48 @@
-import {Component, input, output} from '@angular/core';
+import {Component, input, output, signal} from '@angular/core';
 import {ToDoItem} from '../../../types';
+import {ItemService} from '../../../services/itemService';
+import {SaveButton} from '../saveButton/saveButton';
 
 
 @Component({
   selector: 'to-do-item',
+  imports: [
+    SaveButton
+  ],
   templateUrl: 'todoItem.html'
 })
 export class TodoItem {
+  itemService: ItemService
   item = input.required<ToDoItem>()
+  draftItemText = signal('')
+  isEditing = signal(false)
   onItemComplete = input.required<(id: number) => void>()
+  constructor(service: ItemService) {
+    this.itemService = service
+
+    this.onSave = this.onSave.bind(this)
+  }
 
   completeItem() {
     this.onItemComplete()(this.item().id)
   }
+
+  onEditItem() {
+    this.isEditing.set(true)
+  }
+
+  onTextChange(event: Event) {
+    this.draftItemText.set((event.target as HTMLInputElement).value)
+  }
+
+  onSave() {
+    const { id, ...rest } = this.item()
+    this.itemService.updateItem(id, {
+      ...rest,
+      text: this.draftItemText()
+    })
+    this.isEditing.set(false)
+  }
+
+
 }
