03f446479ef55f2c51554e2c0ae8eb787e499701
Natasha <75773163+natty-light@users.noreply.github.com>
2025-09-09 07:19:25 -0400

    app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from '../components/header/header';
+import {ToDoList} from '../components/todoList/todoList';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [Header, ToDoList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
